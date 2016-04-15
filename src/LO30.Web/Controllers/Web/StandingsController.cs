e316d1f69d208c37ad361a7fe26df41e9826831f
dkhunt27 <huntd@valassis.com>
2016-04-14 22:35:07 -0400

    initial checkin of inspinia template

---
+++ src/LO30.Web/Controllers/Web/StandingsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace LO30.Web.Controllers
+{
+    public class StandingsController : Controller
+    {
+    public IActionResult RegularSeason()
+    {
+
+      ViewData["SubTitle"] = "Welcome in ASP.NET MVC 6 INSPINIA SeedProject ";
+      ViewData["Message"] = "It is an application skeleton for a typical MVC 6 project. You can use it to quickly bootstrap your webapp projects.";
+
+      return View();
+    }
+
+    public IActionResult Playoffs()
+    {
+
+      ViewData["SubTitle"] = "Welcome in ASP.NET MVC 6 INSPINIA SeedProject ";
+      ViewData["Message"] = "It is an application skeleton for a typical MVC 6 project. You can use it to quickly bootstrap your webapp projects.";
+
+      return View();
+    }
+
+  }
+}
